6853b49435108a679e17978eb02187302721dc06
victor-chukwu <okenwa1993@gmail.com>
2021-12-24 11:38:06 +0100

    Made code to start running

--- main.rb
+++ main.rb
@@ -1,27 +1,24 @@
+#!/usr/bin/env ruby
+
 require_relative './person'
+
 require_relative './student'
+
 require_relative './rental'
+
 require_relative './teacher'
+
 require_relative './book'
+
 require_relative './classroom'
 
 class App
   def initialize
     @books = []
-    @rentals = []
-    @people = []
-  end
 
-  def actions
-    puts
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
+    @rentals = []
+
+    @people = []
   end
 
   def start
@@ -57,8 +54,20 @@ class App
     when '6'
       list_rentals
     else
-      puts 'That is not a valid option'
+      puts 'This option is invalid/not available'
     end
+  end
+
+  def actions
+    puts
+    puts 'Enter a corresponding number to chose an option:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts 'end - Exit'
   end
 
   def list_books
@@ -74,27 +83,39 @@ class App
     choice = gets.chomp
 
     case choice
+
     when '1'
+
       create_student
+
     when '2'
+
       create_teacher
+
     else
+
       puts 'That is not a valid input'
+
       nil
+
     end
   end
 
   def create_student
     print 'Age: '
+
     age = gets.chomp
 
     print 'Name: '
+
     name = gets.chomp
 
     print 'Has parent permission? [Y/N]: '
+
     parent_permission = gets.chomp.downcase == 'y'
 
-    student = Student.new(classroom: @classroom, age: age, name: name, parent_permission: parent_permission)
+    student = Student.new(age: age, name: name, parent_permission: parent_permission, classroom: @classroom)
+
     @people.push(student)
 
     puts 'Person created successfully'
@@ -102,15 +123,19 @@ class App
 
   def create_teacher
     print 'Age: '
+
     age = gets.chomp
 
     print 'Name: '
+
     name = gets.chomp
 
     print 'Specialization: '
+
     specialization = gets.chomp
 
-    teacher = Teacher.new(specialization, age, name)
+    teacher = Teacher.new(age: age, name: name, specialization: specialization)
+
     @people.push(teacher)
 
     puts 'Person created successfully'
@@ -118,12 +143,15 @@ class App
 
   def create_book
     print 'Title: '
+
     title = gets.chomp
 
     print 'Author: '
+
     author = gets.chomp
 
     book = Book.new(title, author)
+
     @books.push(book)
 
     puts 'Book created successfully'
@@ -131,21 +159,27 @@ class App
 
   def create_rental
     puts 'Select a book from the following list by number'
+
     @books.each_with_index { |book, i| puts "#{i}) #{book}" }
 
     book_i = gets.chomp.to_i
 
     puts
+
     puts 'Select a person from the following list by number (not ID)'
+
     @people.each_with_index { |person, i| puts "#{i}) #{person}" }
 
     person_i = gets.chomp.to_i
 
     puts
+
     print 'Date: '
+
     date = gets.chomp
 
     rental = Rental.new(@books[book_i], date, @people[person_i])
+
     @rentals.push(rental)
 
     puts 'Rental created successfully'
@@ -153,15 +187,18 @@ class App
 
   def list_rentals
     print 'ID of person: '
+
     id = gets.chomp
 
     puts 'Rentals:'
+
     @rentals.each { |rental| puts rental if rental.person.id == id.to_i }
   end
 end
 
 def main
   app = App.new
+
   app.start
 end
 
